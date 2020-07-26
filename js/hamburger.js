3988b54f27fc85db7d448a4c180684294b99bde6
Marcin Czepiela <czepimm200@gmail.com>
2020-07-26 21:21:07 +0200

    Added hamburger animation

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,10 +4,12 @@ const menu = document.querySelector('#menu');
 hamburger.addEventListener('click', function (e) {
     console.log(menu);
     menu.classList.toggle('d-none');
+    hamburger.classList.toggle('navbar__exit');
 });
 
 menu.querySelectorAll('li').forEach(function (li) {
     li.addEventListener('click', function (e) {
         menu.classList.toggle('d-none');
+        hamburger.classList.toggle('navbar__exit');
     })
 })
